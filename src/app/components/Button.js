0c39b6360192578e74be6290d1d641392aced045
GoodboyBadvibes <eekwebene@gmail.com>
2024-10-01 12:27:39 +0100

    added style paramter for customization

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import style from '../Styles/Button.module.css'
 
-const Button = ({text,className='',fxn=null}) => {
+const Button = ({text,className='',fxn=null, styl={}}) => {
   return (
     <>
-      <div className={'bg-standard w-fit block hover:bg-standarddark cursor-pointer transition-all duration-500 hover:shadow-2xl '+ style.button+' '+className} onClick={fxn}>
+      <div className={'bg-standard w-fit block hover:bg-standarddark cursor-pointer transition-all duration-500 hover:shadow-2xl text-standard '+ style.button+' '+className} style={{...styl}} onClick={fxn}>
         <h1> {text} </h1>
       </div>
     </>
