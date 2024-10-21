3d27778b4fe1746ecc3a1dafa411b3ec96b84932
GoodboyBadvibes <eekwebene@gmail.com>
2024-10-22 00:37:04 +0100

    gallery media query done

--- src/app/components/Gallery.js
+++ src/app/components/Gallery.js
@@ -26,27 +26,30 @@ const gallery =
 
   return (
     <>
-      <div className=' flex justify-between gap-16 items-center'>
+      <div className=' sm:flex flex-row-reverse justify-between gap-16 items-center'>
 
-        <div style={{width:'60%'}}>
-          <div className={' flex justify-between items-center '+ style.gallScroll} id='gallScroll' style={{width:'600px', overflowX:'scroll'}}>
+
+        <div className={' '+ style.gallText}>
+          <div>
+            <h1 className={style.header + ' text-standard sm:text-start text-center pb-10'}>Gallery</h1>
+            <h1 className={style.subHeader + ' sm:text-start text-center pb-10'}>Where and who we've met</h1>
+            <h1 className={style.content}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo neque, illum similique necessitatibus blanditiis tenetur voluptates, reprehenderit, modi nesciunt voluptas eligendi nihil saepe eos error eum? Facere eaque quibusdam perspiciatis vitae tempore nihil atque vel.</h1>
+          </div>
+          <div className='  sm:text-start text-center pt-12' >
+            <i className={'ri-arrow-left-wide-fill cursor-pointer text-standard border-standard ' + style.arrow} onClick={() => { scrollTest('left') }} ></i>
+            <i className={'ri-arrow-right-wide-fill cursor-pointer text-standard border-standard ' + style.arrow} onClick={() => { scrollTest('right') }}></i>
+          </div>
+        </div>
+
+
+
+        <div className={' '+ style.image} style={{}}>
+          <div className={' flex justify-between items-center '+ style.gallScroll} id='gallScroll' style={{overflowX:'scroll'}}>
           {
             gallery.map((pic)=>(
               <Image src={pic.image} width={2500} height={800} style={{scrollSnapAlign:'end'}}/>
             ))
           }
-          </div>
-        </div>
-
-        <div style={{width:'50%'}}>
-          <div>
-            <h1 className={testStyle.header + ' text-standard sm:text-start text-center pb-10'}>Gallery</h1>
-            <h1 className={testStyle.subHeader + ' sm:text-start text-center pb-10'}>Where and who we've met</h1>
-            <h1 className={testStyle.content}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo neque, illum similique necessitatibus blanditiis tenetur voluptates, reprehenderit, modi nesciunt voluptas eligendi nihil saepe eos error eum? Facere eaque quibusdam perspiciatis vitae tempore nihil atque vel.</h1>
-          </div>
-          <div className='  sm:text-start text-center pt-12' >
-            <i className={'ri-arrow-left-wide-fill cursor-pointer text-standard border-standard ' + testStyle.arrow} onClick={() => { scrollTest('left') }} ></i>
-            <i className={'ri-arrow-right-wide-fill cursor-pointer text-standard border-standard ' + testStyle.arrow} onClick={() => { scrollTest('right') }}></i>
           </div>
         </div>
 
